753e71a855131bb591fe2d4a47d08e96fed0d5c8
Bruce Miller <bruce.miller@nist.gov>
2011-03-24 17:47:53 +0000

    Careful of environment-like constructors that don't have an explicit \end control sequence

--- lib/LaTeXML/Box.pm
+++ lib/LaTeXML/Box.pm
@@ -202,9 +202,10 @@ sub setBody {
   $$self{properties}{body} = ($self->isMath ? LaTeXML::MathList->new(@body) : LaTeXML::List->new(@body));
   $$self{properties}{trailer} = $trailer;
   # And copy any otherwise undefined properties from the trailer
-  my %trailerhash = $trailer->getProperties;
-  foreach my $prop (keys %trailerhash){
-    $$self{properties}{$prop} = $trailer->getProperty($prop) unless defined $$self{properties}{$prop}; }
+  if($trailer){
+    my %trailerhash = $trailer->getProperties;
+    foreach my $prop (keys %trailerhash){
+      $$self{properties}{$prop} = $trailer->getProperty($prop) unless defined $$self{properties}{$prop}; }}
   return; }
 
 sub getTrailer  { $_[0]{properties}{trailer}; }
