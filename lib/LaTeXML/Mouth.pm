eaf72261d6001804372f481b8fd50c3b1b38bde4
Bruce Miller <bruce.miller@nist.gov>
2011-07-06 08:26:30 +0000

    add informative stringify method to PerlMouth

--- lib/LaTeXML/Mouth.pm
+++ lib/LaTeXML/Mouth.pm
@@ -445,6 +445,10 @@ sub getSource {
 sub hasMoreInput { 0; }
 sub readToken { undef; }
 
+sub stringify {
+  my($self)=@_;
+  "PerlMouth[$$self{source}]"; }
+
 #**********************************************************************
 1;
 
