67bf6bed570e8e0f423aff2d36250809f8c074f9
Bruce Miller <bruce.miller@nist.gov>
2012-04-10 15:58:42 +0000

    Use improved graphicspath PI; findGraphicFile tries to find the best graphics source to use not just the first one; adds a desirability keyword to the mappings to support this

--- lib/LaTeXML/Post/Graphics.pm
+++ lib/LaTeXML/Post/Graphics.pm
@@ -38,6 +38,12 @@ use base qw(LaTeXML::Post);
 #                         This is useful for getting the best antialiasing for postscript, eg.
 #          unit= (pixel|point) :  What unit the image size is given in.
 #          autocrop     : if the image should be cropped (trimmed) when loaded.
+#          desirability : a number indicating how good of a mapping this entry is.
+#                         This helps choose between two sources that can map to the
+#                         same destination type.
+#                         An entry whose source & destination types are the same
+#                         has desirability=10.
+
 sub new {
   my($class,%options)=@_;
   my $self = $class->SUPER::new(%options);
@@ -57,7 +63,7 @@ sub new {
 	       prescale=>1, ncolors=>'400%', quality=>90, unit=>'point'},
 	ps  =>{destination_type=>'png', transparent=>1, 
 	       prescale=>1, ncolors=>'400%', quality=>90,  unit=>'point'},
-	eps =>{destination_type=>'png', transparent=>1, 
+	eps =>{destination_type=>'png', transparent=>1,
 	       prescale=>1, ncolors=>'400%', quality=>90,  unit=>'point'},
 	jpg =>{destination_type=>'jpg',
 	       ncolors=>'400%', unit=>'pixel'},
@@ -96,9 +102,7 @@ sub findGraphicsPaths {
   my @paths = ();
   foreach my $pi ($doc->findnodes('.//processing-instruction("latexml")')){
     if($pi->textContent =~ /^\s*graphicspath\s*=\s*([\"\'])(.*?)\1\s*$/){
-      my $value=$2;
-      while($value=~ s/^\s*\{(.*?)\}//){
-	push(@paths,$1); }}}
+      push(@paths,$2); }}
   @paths; }
 
 # Return a list of ZML nodes which have graphics that need processing.
@@ -114,9 +118,21 @@ sub getGraphicsSourceTypes {
 sub findGraphicFile {
   my($self,$doc,$node)=@_;
   if(my $name = $node->getAttribute('graphic')){
-    pathname_find($name,paths=>$LaTeXML::Post::Graphics::SEARCHPATHS,
-		  # accept empty type, incase bad type name, but actual file's content is known type.
-		  types=>['',$self->getGraphicsSourceTypes]); }
+    # Find all acceptable image files, in order of search paths
+    my @paths = pathname_findall($name,paths=>$LaTeXML::Post::Graphics::SEARCHPATHS,
+				 # accept empty type, incase bad type name, but actual file's content is known type.
+				 types=>['',$self->getGraphicsSourceTypes]);
+    my($best,$bestpath)=(-1,undef);
+    # Now, find the first image that is either the correct type,
+    # or has the most desirable type mapping
+    foreach my $path (@paths){
+      my $type = pathname_type($path);
+      my $props = $$self{typeProperties}{$type};
+      my $desirability = $$props{desirability} || ($type eq $$props{destination_type} ? 10 : 0);
+      if($desirability > $best){
+	$best = $desirability;
+	$bestpath = $path; }}
+    $bestpath; }
   else {
     undef; }}
 
