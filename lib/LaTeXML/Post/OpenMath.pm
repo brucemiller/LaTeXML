e4334d0b5e9147423a6efc2fbee33c7489b85a33
Bruce Miller <bruce.miller@nist.gov>
2012-12-11 17:29:37 +0000

    update for new process API & error messages

--- lib/LaTeXML/Post/OpenMath.pm
+++ lib/LaTeXML/Post/OpenMath.pm
@@ -26,6 +26,7 @@ package LaTeXML::Post::OpenMath;
 use strict;
 use LaTeXML::Common::XML;
 use LaTeXML::Post::MathML;
+use LaTeXML::Post;
 use base qw(LaTeXML::Post::MathProcessor);
 
 our $omURI = "http://www.openmath.org/OpenMath";
@@ -48,7 +49,8 @@ sub convertNode {
   my($self,$doc,$xmath,$style)=@_;
   my($item,@rest)=  element_nodes($xmath);
   if(@rest){			# Unparsed ???
-    print STDERR "Warning: got extra nodes for content!\n  ".$xmath->toString."\n" if @rest; }
+    Warn('unexpected','content',undef,
+	 "Got extra nodes for math content:".$xmath->toString) if @rest; }
   Expr($item); }
 
 sub combineParallel {
@@ -95,7 +97,8 @@ sub Expr_aux {
   my $tag = getQName($node);
   if(($tag eq 'ltx:XMath') || ($tag eq 'ltx:XMWrap')){
     my($item,@rest)=  element_nodes($node);
-    print STDERR "Warning: got extra nodes for content!\n  ".$node->toString."\n" if @rest;
+    Warn('unexpected','content',undef,
+	 "Got extra nodes for content: ".$node->toString) if @rest;
     Expr($item); }
   elsif($tag eq 'ltx:XMDual'){
     my($content,$presentation) = element_nodes($node);
