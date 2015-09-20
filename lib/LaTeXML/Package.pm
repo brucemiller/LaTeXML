a92e4722b48ed9506768c4b2fb653ca26b7532d4
Bruce Miller <bruce.miller@nist.gov>
2015-09-20 17:44:01 -0400

    rename package/class loading hook to end with -h@@k which is the more correct name

--- lib/LaTeXML/Package.pm
+++ lib/LaTeXML/Package.pm
@@ -2020,7 +2020,7 @@ my $inputdefinitions_options = {    # [CONSTANT]
   type => 1, as_class => 1, noltxml => 1, notex => 1, noerror => 1, after => 1 };
 #   options=>[options...]
 #   withoptions=>boolean : pass options from calling class/package
-#   after=>code or tokens or string as $name.$type-hook macro. (executed after the package is loaded)
+#   after=>code or tokens or string as $name.$type-h@@k macro. (executed after the package is loaded)
 # Returns the path that was loaded, or undef, if none found.
 sub InputDefinitions {
   my ($name, %options) = @_;
@@ -2061,7 +2061,7 @@ sub InputDefinitions {
       PassOptions($name, $astype, @{ $options{options} || [] });    # passed explicit options.
              # Note which packages are pretending to be classes.
       PushValue('@masquerading@as@class', $name) if $options{as_class};
-      DefMacroI(T_CS("\\$name.$astype-hook"), undef, $options{after} || '');
+      DefMacroI(T_CS('\\' . $name . '.' . $astype . '-h@@k'), undef, $options{after} || '');
       DefMacroI(T_CS('\opt@' . $name . '.' . $astype), undef,
         Tokens(Explode(join(',', @{ LookupValue('opt@' . $name . "." . $astype) }))));
     }
@@ -2072,7 +2072,7 @@ sub InputDefinitions {
     else {
       loadTeXDefinitions($filename, $file); }
     if ($options{handleoptions}) {
-      Digest(T_CS("\\$name.$astype-hook"));
+      Digest(T_CS('\\' . $name . '.' . $astype . '-h@@k'));
       DefMacroI('\@currname', undef, Tokens(Explode($prevname))) if $prevname;
       DefMacroI('\@currext',  undef, Tokens(Explode($prevext)))  if $prevext;
       # Add an appropriately faked entry into \@filelist
@@ -3384,7 +3384,7 @@ specifies a list of options (in the 'package options' sense) to be passed
 
 =item C<after=E<gt>I<tokens> | I<code>($gullet)>
 
-provides I<tokens> or I<code> to be processed by a C<I<name>.I<type>-hook> macro.
+provides I<tokens> or I<code> to be processed by a C<I<name>.I<type>-h@@k> macro.
 
 =item C<as_class=E<gt>I<boolean>>
 
