f31bb2924a478b36f05653574feb3d39b13f35e6
Bruce Miller <bruce.miller@nist.gov>
2015-11-06 15:47:14 -0500

    remove obsolete mention of glossarymark

--- lib/LaTeXML/Post/Scan.pm
+++ lib/LaTeXML/Post/Scan.pm
@@ -352,7 +352,7 @@ sub indexmark_handler {
     $entry->noteAssociation(referrers => $parent_id => ($node->getAttribute('style') || 'normal')); }
   return; }
 
-# This handles glossarymark and glossaryentry
+# This handles glossaryentry
 sub glossaryentry_handler {
   my ($self, $doc, $node, $tag, $parent_id) = @_;
   my $id   = $node->getAttribute('xml:id');
