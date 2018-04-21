94903dc6d553794968b19ffa128176afa676cdcf
Bruce Miller <bruce.miller@nist.gov>
2018-04-21 15:01:33 -0400

    further improve \end{document} to handle unfinished conditionals

--- lib/LaTeXML/Core.pm
+++ lib/LaTeXML/Core.pm
@@ -158,6 +158,7 @@ sub finishDigestion {
   my @stuff   = ();
   while ($stomach->getGullet->getMouth->hasMoreInput) {
     push(@stuff, $stomach->digestNextBody); }
+  # Note that \end{document} will generally handle these cases as a Warning
   if (my $env = $state->lookupValue('current_environment')) {
     Error('expected', "\\end{$env}", $stomach,
       "Input ended while environment $env was open"); }
