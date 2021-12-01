875b983dacee50d7a3958e17aa1642e44472e7b6
Bruce Miller <nebconinc@gmail.com>
2021-12-01 17:24:26 -0500

    MuDimension and MuGlue are also fixedpoint, but represent scaled mu, not pt; thus also need an spValue method to get the length in scaled points

--- lib/LaTeXML/Core/MuDimension.pm
+++ lib/LaTeXML/Core/MuDimension.pm
@@ -13,28 +13,56 @@ package LaTeXML::Core::MuDimension;
 use LaTeXML::Global;
 use strict;
 use warnings;
+use LaTeXML::Common::Number;
+use LaTeXML::Common::Dimension;
 use base qw(LaTeXML::Common::Dimension);
 use base qw(Exporter);
 our @EXPORT = (qw(&MuDimension));
 
 #======================================================================
 # Exported constructor.
+# Note that MuDimension is almost never used in TeX (there's no \newmudimen)
 
+# Create a MuDimension given either a float with "mu" OR a number in scaled mu (NOT scaled points!)
 sub MuDimension {
-  my ($scaledpoints) = @_;
-  return LaTeXML::Core::MuDimension->new($scaledpoints); }
+  my ($spec) = @_;
+  return LaTeXML::Core::MuDimension->new($spec); }
+
+sub _unit { return 'mu'; }
+
+sub new {
+  my ($class, $spec) = @_;
+  $spec = "0" unless $spec;
+  $spec = ToString($spec) if ref $spec;
+  if ($spec =~ /^(-?\d*\.?\d*)mu$/) {    # mu given, convert to scaled mu
+    return bless [fixedpoint($1, $UNITY)], $class; }    # fake "mu" in sp
+  else {
+    # See comment in Dimension for why kround rather than int
+    return bless [kround($spec || 0)], $class; } }
 
 #======================================================================
-
-# A mu is 1/18th of an em in the current math font.
-# 1 mu = 1em/18 = 10pt/18 = 5/9 pt; 1pt = 9/5mu = 1.8mu
-sub toString {
-  my ($self) = @_;
-  return LaTeXML::Common::Float::floatformat($$self[0] / 65536 * 1.8) . 'mu '; }
-
 sub stringify {
   my ($self) = @_;
   return "MuDimension[" . $$self[0] . "]"; }
+
+# Note that $mu->valueOf will return scaled mu, NOT scaled pts;
+# But in priniple these should never be combined with regular Dimensions/Glue
+# TeX gives error "Incompatible glue units"
+# HOWEVER, since we'll encounter mixtures of mu & pts when computing sizes,
+# we're going to have to make sure we use the right scaling!
+sub spValue {
+  my ($self, $prec) = @_;
+  return kround($$self[0] * $STATE->lookupValue('font')->getMUWidth / $UNITY); }
+
+# A mu is 1/18th of an em in the current math font.
+# 1 mu = 1em/18 = 10pt/18 = 5/9 pt; 1pt = 9/5mu = 1.8mu (for 10pt)
+sub ptValue {
+  my ($self, $prec) = @_;
+  return roundto($$self[0] / $UNITY / 1.8, $prec); }
+
+sub pxValue {
+  my ($self, $prec) = @_;
+  return roundto($$self[0] / $UNITY / 1.8 * ($STATE->lookupValue('DPI') || 100 / 72.27), $prec); }
 
 #======================================================================
 1;
@@ -52,9 +80,11 @@ extends L<LaTeXML::Common::Dimension>.
 
 =over 4
 
-=item C<< $mudimension = MuDimension($dim); >>
+=item C<< $mudimension = MuDimension($spec); >>
 
-Creates a MuDimension object; similar to Dimension.
+Creates a MuDimension object, similar to Dimension.
+C<$spec> can be a string with a floating point number and "mu"
+or just a number standing for scaled mu (ie. fixedpoint).
 
 =back
 
--- lib/LaTeXML/Core/MuGlue.pm
+++ lib/LaTeXML/Core/MuGlue.pm
@@ -13,7 +13,11 @@ package LaTeXML::Core::MuGlue;
 use LaTeXML::Global;
 use strict;
 use warnings;
+use LaTeXML::Common::Error;
 use LaTeXML::Common::Float;
+use LaTeXML::Common::Number;
+use LaTeXML::Common::Dimension;
+use LaTeXML::Common::Glue;
 use base qw(LaTeXML::Common::Glue);
 use base qw(Exporter);
 our @EXPORT = (qw(&MuGlue));
@@ -21,41 +25,33 @@ our @EXPORT = (qw(&MuGlue));
 #======================================================================
 # Exported constructor.
 
+# Create a new MuGlue, given EITHER a string with muunits, fills,etc,
+# OR separate args, wiith $spec, $plus, $minus being fixed point,
+# and $pfill, $mfill being 0 (sp) or a fillcode
+# Note: this stores mu (NOT pts) as fixed point number
+# [Glue is parameterized on the effective unit]
 sub MuGlue {
-  my ($scaledpoints, $plus, $pfill, $minus, $mfill) = @_;
-  return LaTeXML::Core::MuGlue->new($scaledpoints, $plus, $pfill, $minus, $mfill); }
+  my ($spec, $plus, $pfill, $minus, $mfill) = @_;
+  return LaTeXML::Core::MuGlue->new($spec, $plus, $pfill, $minus, $mfill); }
 
 #======================================================================
-
-# 1 mu = 1em/18 = 10pt/18 = 5/9 pt; 1pt = 9/5mu = 1.8mu
-sub toString {
-  my ($self) = @_;
-  my ($sp, $plus, $pfill, $minus, $mfill) = @$self;
-  my $string = LaTeXML::Common::Float::floatformat($sp / 65536 * 1.8) . 'mu ';
-  $string .= 'plus ' . ($pfill
-    ? $plus . $LaTeXML::Common::Glue::FILL[$pfill]
-    : LaTeXML::Common::Float::floatformat($plus / 65536 * 1.8) . 'mu ') if $plus != 0;
-  $string .= 'minus ' . ($mfill
-    ? $minus . $LaTeXML::Common::Glue::FILL[$mfill]
-    : LaTeXML::Common::Float::floatformat($minus / 65536 * 1.8) . 'mu ') if $minus != 0;
-  return $string; }
-
-sub toAttribute {
-  my ($self) = @_;
-  my ($sp, $plus, $pfill, $minus, $mfill) = @$self;
-  my $string = LaTeXML::Common::Float::floatformat($sp / 65536 * 1.8) . "mu";
-  $string .= ' plus ' . ($pfill
-    ? $plus . $LaTeXML::Common::Glue::FILL[$pfill]
-    : LaTeXML::Common::Float::floatformat($plus / 65536 * 1.8) . 'mu') if $plus != 0;
-  $string .= ' minus ' . ($mfill
-    ? $minus . $LaTeXML::Common::Glue::FILL[$mfill]
-    : LaTeXML::Common::Float::floatformat($minus / 65536 * 1.8) . 'mu') if $minus != 0;
-  return $string; }
+sub _unit { return 'mu'; }
 
 sub stringify {
   my ($self) = @_;
   return "MuGlue[" . join(',', @$self) . "]"; }
 
+sub spValue {
+  my ($self, $prec) = @_;
+  return fixpoint($$self[0] / $UNITY, $STATE->lookupValue('font')->getMUWidth); }
+
+sub ptValue {
+  my ($self, $prec) = @_;
+  return roundto($self->spValue / $UNITY, $prec); }
+
+sub pxValue {
+  my ($self, $prec) = @_;
+  return roundto($self->spValue / $UNITY / 1.8 * ($STATE->lookupValue('DPI') || 100 / 72.27), $prec); }
 #======================================================================
 1;
 
@@ -72,11 +68,16 @@ extends L<LaTeXML::Common::Glue>.
 
 =over 4
 
-=item C<< $glue = MuGlue($gluespec); >>
+=item C<< $glue = MuGlue($spec); >>
 
-=item C<< $glue = MuGlue($sp,$plus,$pfill,$minus,$mfill); >>
+=item C<< $glue = MuGlue($spec,$plus,$pfill,$minus,$mfill); >>
 
-Creates a MuGlue object, similar to Glue.
+Creates a MuGlue object (similar to Glue).  C<$spec> can be a string in the
+form that TeX recognizes (number "mu" optional plus and minus parts).
+Alternatively, the dimension, plus and minus parts can be given separately
+as scaled mu (fixpoint),
+while C<$pfill> and C<$mfill> are 0 (when the C<$plus> or C<$minus> part is in scaledmu)
+or 1,2,3 for fil, fill or filll, respectively.
 
 =back
 
