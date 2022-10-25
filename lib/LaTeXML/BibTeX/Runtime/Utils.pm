d771249c95619e6d49c5019e8e67801e086a6a71
Bruce Miller <nebconinc@gmail.com>
2022-10-25 16:59:17 -0400

    Improve concatString to better join sources when concatenating

--- lib/LaTeXML/BibTeX/Runtime/Utils.pm
+++ lib/LaTeXML/BibTeX/Runtime/Utils.pm
@@ -17,7 +17,16 @@ use base qw(Exporter);
 our @EXPORT = qw(
   &concatString &simplifyString &applyPatch
   &fmtType
+  &showStrings
 );
+
+sub showStrings {
+  my ($strings, $sources) = @_;
+  my @str = @$strings;
+  my @src = @$sources;
+  return join(', ',
+    map { "'" . $str[$_] . "'[" . (defined $src[$_] ? $src[$_] : '<none>') . ']'; }
+      0 .. $#str); }
 
 # given two runtime strings, join them and their sources together
 # and return a new runtime string
@@ -25,31 +34,42 @@ sub concatString {
   my ($stringA, $sourceA, $stringB, $sourceB) = @_;
   # join the strings and sources
   # Note: Both strings and sources are copies at this point and can be treated as mutable.
-  my @strings    = (@$stringA, @$stringB);
-  my @sources    = (@$sourceA, @$sourceB);
-  my @theString  = ();
-  my @theSources = ();
-  my ($hasElement, $string, $source, $prevSource) = (0);
+  my @strings = (@$stringA, @$stringB);
+  # Make sure we have sources (even undefined ones) corresponding to EACH string entry
+  my @sources = ((defined $sourceA ? @$sourceA : map { (undef); } @$stringA),
+    (defined $sourceB ? @$sourceB : map { (undef); } @$stringB));
+  ##Debug("CONCAT : " . showStrings([@strings],[@sources]));
+  my @newstrings = ();
+  my @newsources = ();
+  my ($string, $source);
   while (defined($string = shift(@strings))) {
     $source = shift(@sources);
-    # if we have a non-empty string
-    unless ($string eq '') {
-      # we can join element iff the following conditions are met
-      # - we have at least one source already
-      # - the previous element has no source
-      # - the current element has no source
-      if ($hasElement && !defined($prevSource) && !defined($source)) {
-        $theString[-1] .= $string; }
-      # in any other case, we need to setup a new element in the result
-      else {
-        push(@theString,  $string);
-        push(@theSources, $source);
-        $prevSource = $source;
-        $hasElement = 1; } } }
+    next if $string eq '';
+    # else push it
+    # if no previous entry, just push this one
+    if (!scalar(@newstrings)) {    # No previous entry, so just push this one
+      push(@newstrings, $string);
+      push(@newsources, $source); }
+    # if previous is same source (even undef), join them
+    elsif ((defined $source
+        ? (defined $newsources[-1]) && ($source eq $newsources[-1])
+        : !defined $newsources[-1])) {
+      $newstrings[-1] .= $string; }    # Just combine them
+        # if previous source is undef, but before that source is same (defined) source, join all three
+    elsif ((defined $source) && (!defined $newsources[-1])
+      && (defined $newsources[-2]) && ($source eq $newsources[-2])) {
+      $newstrings[-2] .= $newstrings[-1] . $string;
+      pop(@newstrings); pop(@newsources); }
+    # Else, append new entry
+    else {
+      push(@newstrings, $string);
+      push(@newsources, $source); } }
   # and return the strings and sources
-  return [@theString], [@theSources]; }
+  ## Debug(" ===>  " . showStrings([@newstrings],[@newsources]));
+  return [@newstrings], [@newsources]; }
 
 # given a runtime string turn it into a single string and useful source
+# NOTE: This LOSES sources!
 sub simplifyString {
   my ($string, $sources) = @_;
   # return the first 'defined' source
@@ -76,6 +96,7 @@ sub simplifyString {
 #              old string.
 # Whenever the patch() function returns the original string, the original source
 # references are maintained in their entirety.
+# NOTE: This LOSES sources!
 sub applyPatch {
   my ($oldString, $oldSource, $patch, $semantics) = @_;
   # simplify the old string
