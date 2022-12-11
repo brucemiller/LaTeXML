38fa27ba5539dbfe576dbd8e040df132cf3322b5
Vincenzo Mantova <1962985+xworld21@users.noreply.github.com>
2022-12-11 18:27:20 +0000

    use SOURCE_DATE_EPOCH instead of current time (#1920)

--- lib/LaTeXML/Post/Manifest/Epub.pm
+++ lib/LaTeXML/Post/Manifest/Epub.pm
@@ -131,7 +131,7 @@ sub initialize {
   $language->appendText($document_language);
   my $modified = $metadata->addNewChild(undef, "meta");
   $modified->setAttribute('property', 'dcterms:modified');
-  my $now_string = strftime "%Y-%m-%dT%H:%M:%SZ", gmtime;    # CCYY-MM-DDThh:mm:ssZ
+  my $now_string = strftime "%Y-%m-%dT%H:%M:%SZ", gmtime($ENV{SOURCE_DATE_EPOCH} // time); # CCYY-MM-DDThh:mm:ssZ
   $modified->appendText($now_string);
   my $identifier = $metadata->addNewChild("http://purl.org/dc/elements/1.1/", "identifier");
   $identifier->setAttribute('id', 'pub-id');
