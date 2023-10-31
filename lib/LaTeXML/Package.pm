c4201dd5a99d7d9d96e3e2185b0b884c753d4415
Bruce Miller <nebconinc@gmail.com>
2023-10-31 18:22:51 -0400

    IsEmpty should NOT count spaces as empty; define new IsEmptyOrSpace in case that's actually needed

--- lib/LaTeXML/Package.pm
+++ lib/LaTeXML/Package.pm
@@ -82,7 +82,7 @@ our @EXPORT = (qw(&DefAutoload &DefExpandable
   qw(&Expand &Invocation &Digest &DigestText &DigestIf &DigestLiteral
     &RawTeX &Let &StartSemiverbatim &EndSemiverbatim
     &Tokenize &TokenizeInternal
-    &IsEmpty),
+    &IsEmpty &IsEmptyOrSpace),
 
   # Font encoding
   qw(&DeclareFontMap &FontDecode &FontDecodeString &LoadFontMap),
@@ -974,9 +974,8 @@ sub TokenizeInternal {
   local $STATE = $STY_CATTABLE;
   return LaTeXML::Core::Mouth->new($string)->readTokens; }
 
-# Check whether all these things are "empty" (spaces are empty!!)
+# Check whether all these things are "empty" (spaces are NOT empty!!)
 # short-circuit: return 0 quickly if anything is NOT empty.
-# Will we need to distinguish between "empty" and "not visible"??
 sub IsEmpty {
   my (@things) = @_;
   foreach my $thing (@things) {
@@ -988,8 +987,31 @@ sub IsEmpty {
     elsif ($ref eq 'LaTeXML::Core::Token') {
       my $cc = $$thing[1];
       return 0 if ($cc == CC_LETTER) || ($cc == CC_OTHER) || ($cc == CC_ACTIVE) || ($cc == CC_CS); }
+    elsif (!$thing->getProperty('isEmpty')) {
+      if ($ref eq 'LaTeXML::Core::Box') {
+        my $s = $thing->getString;
+        return 0 if (defined $s) && length($s); }
+      elsif ($ref eq 'LaTeXML::Core::List') {
+        return 0 unless IsEmpty($thing->unlist); }
+      elsif ($ref eq 'LaTeXML::Core::Whatsit') {
+        return 0 unless ($thing->getDefinition eq $STATE->lookupDefinition(T_BEGIN))
+          && IsEmpty($thing->getBody->unlist); } } }
+  return 1; }
+
+# Check whether all these things are "empty" OR spaces
+sub IsEmptyOrSpace {
+  my (@things) = @_;
+  foreach my $thing (@things) {
+    my $ref = ref $thing;
+    if    (!$thing)                          { }
+    elsif ($ref eq 'LaTeXML::Core::Comment') { }
+    elsif ($ref eq 'LaTeXML::Core::Tokens') {
+      return 0 unless IsEmpty($thing->unlist); }
+    elsif ($ref eq 'LaTeXML::Core::Token') {
+      my $cc = $$thing[1];
+      return 0 if ($cc == CC_LETTER) || ($cc == CC_OTHER) || ($cc == CC_ACTIVE) || ($cc == CC_CS); }
     elsif ((!$thing->getProperty('isEmpty'))
-      && (!$thing->getProperty('isSpace'))) {    # A space-like thing
+      && (!$thing->getProperty('isSpace'))) {
       if ($ref eq 'LaTeXML::Core::Box') {
         my $s = $thing->getString;
         return 0 if (defined $s) && ($s !~ /^\s*$/); }
