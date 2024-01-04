cbfd376d0094bfd65c91a3997cfb247f6acaa368
Deyan Ginev <deyan.ginev@gmail.com>
2024-01-03 19:22:05 -0500

    allow () paren chars in defensive path check

--- lib/LaTeXML/Package.pm
+++ lib/LaTeXML/Package.pm
@@ -2139,7 +2139,7 @@ sub FindFile_fallback {
 
 sub pathname_is_nasty {
   my ($pathname) = @_;
-  return $pathname =~ /[^\w\-_\+\=\/\\\.~\:\s]/; }
+  return $pathname =~ /[^\w\-_\+\=\/\\\.~\:\s)(]/; }
 
 sub maybeReportSearchPaths {
   if (LookupValue('SEARCHPATHS_REPORTED')) {
