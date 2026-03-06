9d4a120804a4a47b66c8b5b622ca29b1f09460e7
Deyan Ginev <deyan.ginev@gmail.com>
2026-03-06 17:20:17 -0500

    patch roman_aux to correctly compute '\romannumeral 1000' as 'm'

--- lib/LaTeXML/Package.pm
+++ lib/LaTeXML/Package.pm
@@ -455,7 +455,7 @@ sub roman_aux {
   # TeX proper returns empty on negative integers
   return '' unless $n && ($n > 0);
   my $div = 1000;
-  my $s   = ($n > $div ? ('m' x int($n / $div)) : '');
+  my $s   = ($n >= $div ? ('m' x int($n / $div)) : '');
   my $p   = 4;
   while ($n %= $div) {
     $div /= 10;
@@ -952,7 +952,7 @@ sub Expand {
   return () unless @tokens;
   my $gullet = $STATE->getStomach->getGullet;
   return $gullet->readingFromMouth(Tokens(T_BEGIN, @tokens, T_END), sub {
-    $gullet->readBalanced(2, 0, 1); }); }
+      $gullet->readBalanced(2, 0, 1); }); }
 
 # Return $tokens, partially expanded (defer protected, and results of \the)
 sub ExpandPartially {
@@ -960,7 +960,7 @@ sub ExpandPartially {
   return () unless @tokens;
   my $gullet = $STATE->getStomach->getGullet;
   return $gullet->readingFromMouth(Tokens(T_BEGIN, @tokens, T_END), sub {
-    $gullet->readBalanced(1, 0, 1); }); }
+      $gullet->readBalanced(1, 0, 1); }); }
 
 sub Invocation {
   my ($token, @args) = @_;
@@ -1371,7 +1371,7 @@ sub LookupDimension {
       return $defn->valueOf; }
     else {
       return $STATE->getStomach->getGullet->readingFromMouth($cs, sub {
-        $_[0]->readDimension; }); } }
+          $_[0]->readDimension; }); } }
   else {
     Warn('expected', 'register', $STATE->getStomach,
       "The control sequence " . ToString($cs) . " is not a register"); }
@@ -2952,11 +2952,11 @@ sub decodeMathChar {
       elsif ($fontdef = LookupValue('textfont_' . $fam))         { $downsize = 2; } }
     my $defn = $STATE->lookupDefinition($fontdef);
     $fontinfo = $defn && $defn->isFontDef;
-    if(! $fontinfo) {
-      $defn = $STATE->lookupDefinition(T_CS('\lx@default@font'));
+    if (!$fontinfo) {
+      $defn     = $STATE->lookupDefinition(T_CS('\lx@default@font'));
       $fontinfo = $defn && $defn->isFontDef; }
     if ($fontinfo && (ref $fontinfo eq 'HASH')
-        && $basefontinfo && ($$basefontinfo{size} != $curfont->getSize)) {
+      && $basefontinfo && ($$basefontinfo{size} != $curfont->getSize)) {
       # If we've gotten an explicit font SIZE change; Adjust!
       $fontinfo = {%$fontinfo}; $$fontinfo{size} = $curfont->getSize; } }
   my $font = $curfont;
